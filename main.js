21624488a490fd05c5a9ae4db9f9b4171e229795
Alexander Gau <alexandergau07@gmail.com>
2025-10-23 19:12:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x1a1a1a);
 
 const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(-2.0635, 1.5, 1.15);
+camera.position.set(-2.0635, 1.5, 1);
 camera.lookAt(-1.75, 1, 0);
 
 const renderer = new THREE.WebGLRenderer({
