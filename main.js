f20798c24f7cbcc72dfab39b2a8c198cd50a8e40
Alexander Gau <alexandergau07@gmail.com>
2025-10-26 16:27:28 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ window.scene = scene;
 
 // Camera setup (static position)
 const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(.6, 1.5, 0); // static starting position
+camera.position.set(.6, 1.3, 0); // static starting position
 camera.lookAt(.6, 1.3, 0);         // static look direction
 
 // Renderer
