8e71317474bff0a9c5fd719cea4af4b81f49308f
Alexander Gau <alexandergau07@gmail.com>
2025-10-26 14:53:56 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ window.scene = scene; // expose for console debugging
 
 // Camera setup
 const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
-const startPosition = new THREE.Vector3(-15, 5, 10); // dramatic starting angle
+const startPosition = new THREE.Vector3(-15, 1.5, .95); // dramatic starting angle
 const endPosition = new THREE.Vector3(0, 1.5, 0.95); // final resting spot
 camera.position.copy(startPosition);
 camera.lookAt(5, 1, 0);
